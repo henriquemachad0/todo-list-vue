2132acdb8109d672fcdcc8889b601b4ba0725726
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-05-22 13:37:04 -0300

    feat: add todos in local storage

--- scripts.js
+++ scripts.js
@@ -14,6 +14,7 @@ const todosApp = {
                 this.newTodo = {
                     done: false
                 }    
+                localStorage.setItem("todos", JSON.stringify(this.todos))
             } else {
                 alert('O texto da tarefa é obrigatório')
             }
